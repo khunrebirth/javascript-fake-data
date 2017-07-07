e552d1e823a2932d5f925e7a3266db9c7e3582b9
ThanuSin <khunrebirth@gmail.com>
2017-07-07 10:36:58 +0700

    Add Screenshot

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
-let faker = require('faker');
+let faker = require('faker'); // Document Faker (https://github.com/marak/Faker.js/)
 
 module.exports = function() {
   let data = { users: [] }
   for (let i = 1; i <= 100; i++) {
-    data.users.push({ id: i, name: faker.name.findName() });
+    data.users.push({ 
+      id: i, 
+      name: faker.name.findName(), 
+      job: faker.name.jobTitle(),
+      phone: faker.phone.phoneNumber()
+    });
   }
   return data;
 }
